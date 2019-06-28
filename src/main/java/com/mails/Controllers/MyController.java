92a5938799a1b5de0f3d7ea5b85760e1dd75744e
gq544 <woshigq513@163.com>
2019-06-28 23:07:59 +0800

    mymail

--- src/main/java/com/mails/Controllers/MyController.java
+++ src/main/java/com/mails/Controllers/MyController.java
@@ -1,9 +1,15 @@
 package com.mails.Controllers;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 //主Controller请求
 @Controller
@@ -21,9 +27,18 @@ public class MyController {
 
     @PostMapping("/loginrup")
     @ResponseBody
-    public boolean loginok(){
+    public  void loginok(@RequestParam("callback")String callback, HttpServletResponse resp){
         System.out.println("进了");
-        return true;
+        String str= JSON.toJSONString(true);
+        resp.setContentType("text/json;charset=UTF-8");
+        PrintWriter pw = null;
+        try {
+            pw = resp.getWriter();
+            pw.println();
+            pw.println(callback+"("+str+")");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping("/toindex")
