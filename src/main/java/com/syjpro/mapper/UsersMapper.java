5bb22879b7bfeb6f85f8e9e4473e196987b2a688
gq544 <woshigq513@163.com>
2019-06-29 13:41:47 +0800

    chang

---
+++ src/main/java/com/syjpro/mapper/UsersMapper.java
@@ -0,0 +1,17 @@
+package com.syjpro.mapper;
+
+import com.syjpro.entity.Users;
+
+public interface UsersMapper {
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    Users selectByPrimaryKey(Integer userid);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
