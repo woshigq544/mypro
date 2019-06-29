841b18d9e8fb5be9f7229bd8ac560bb4ea5a8b47
gq544 <woshigq513@163.com>
2019-06-29 14:05:16 +0800

    chang

--- src/main/java/com/syjpro/Controllers/MyController.java
+++ src/main/java/com/syjpro/Controllers/MyController.java
@@ -2,19 +2,23 @@ package com.syjpro.Controllers;
 
 import com.alibaba.fastjson.JSON;
 import com.syjpro.entity.Users;
+import com.syjpro.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 //主Controller请求
 @Controller
 public class MyController {
+    @Resource
+    private UserService userServiceImpl;
+
     @RequestMapping("/login")
     public String commin(){
 
