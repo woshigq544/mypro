c31d755bfd7d3f58a756d709b6854d1f64e54360
gq544 <woshigq513@163.com>
2019-06-30 17:01:12 +0800

    mymail

---
+++ src/main/java/com/syjpro/config/MybatisConfig.java
@@ -0,0 +1,33 @@
+package com.syjpro.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/*@Configuration
+@MapperScan(basePackages = {"com.syjpro.mapper"})
+public class MybatisConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactoryBean sqlSessionFactory(ApplicationContext applicationContext) throws IOException {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeAliasesPackage("com.syjpro.mapper");
+        Resource[] resources = new PathMatchingResourcePatternResolver()
+                .getResources("classpath:mappers/*.xml");
+        sessionFactory.setMapperLocations(resources);
+
+        return sessionFactory;
+    }
+}*/
